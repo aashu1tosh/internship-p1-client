08091a4d2e661abddd52e07fb50024781a30bdb1
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-05-29 15:54:00 +0545

    perf: authorization headers added so, we should no longer pass it on every method that is called

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -2,7 +2,10 @@ import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_BACKEND_URL,
-    timeout: import.meta.env.VITE_APP_TIME_OUT
+    timeout: import.meta.env.VITE_APP_TIME_OUT,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+    }
 })
 
 export default instance

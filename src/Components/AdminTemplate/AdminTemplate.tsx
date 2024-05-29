35c64edfa39610e085a54d14ff344bc57d86f56d
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-05-29 15:57:15 +0545

    style: Made sure the main-contents aren't overlapped by the navbar and sidebar

--- src/Components/AdminTemplate/AdminTemplate.tsx
+++ src/Components/AdminTemplate/AdminTemplate.tsx
@@ -1,12 +1,16 @@
+import Sidebar from 'Components/Sidebar/Sidebar'
 import UserNavbar from 'Components/UserNavbar/UserNavbar'
 import { Outlet } from 'react-router-dom'
-
+import './AdminTemplate.css'
 const AdminTemplate = () => {
     return (
-        <>
+        <div>
             <UserNavbar />
-            <Outlet />
-        </>
+            <Sidebar />
+            <div className='main-content'>
+                <Outlet />
+            </div>
+        </div>
     )
 }
 

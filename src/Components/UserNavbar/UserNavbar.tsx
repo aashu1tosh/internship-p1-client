c237fff921fd8943369423ff49719c9294930b77
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-05-29 16:07:46 +0545

    feat: Toggle Dark Light Theme button added to UserNavbar

--- src/Components/UserNavbar/UserNavbar.tsx
+++ src/Components/UserNavbar/UserNavbar.tsx
@@ -1,23 +1,23 @@
 import { image } from '@config/constant/image';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import ThemeToggleButton from 'ui/atom/ThemeToggleButton/ThemeToggleButton';
 import './UserNavbar.css';
 
 const UserNavbar: React.FC = () => {
-    const navigate = useNavigate();
-    const logout = () => {
-        localStorage.removeItem("accessToken");
-        navigate('/signin')
-    }
+
     return (
         <div id="unique-navbar">
-            <div className="navbar-logo">
-                <img src={image?.logo} alt="Logo" />
-            </div>
+            <div></div>
+            <Link to='/'>
+                <div className="navbar-logo">
+                    <img src={image?.logo} alt="Logo" />
+                </div>
+            </Link>
+
             <div className="navbar-links">
-                <li>Home</li>
-                <li>Dashboard</li>
-                <li onClick={logout}>Logout</li>
+                {/* <li>Logout</li> */}
+                <li><ThemeToggleButton /></li>
             </div>
         </div>
     );

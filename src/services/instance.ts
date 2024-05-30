924166e33e2791b5d891f20bb1f34db8960fd540
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-05-30 10:12:59 +0545

    added: Authorization Header added at axios instance

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,10 +1,12 @@
+import EncryptDecrypt from "@functions/EncryptDecrypt";
 import axios, { AxiosInstance } from "axios";
+
 
 const instance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_BACKEND_URL,
     timeout: import.meta.env.VITE_APP_TIME_OUT,
     headers: {
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+        Authorization: `Bearer ${EncryptDecrypt.decrypt(localStorage.getItem("accessToken") as string)}`
     }
 })
 

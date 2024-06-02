0b74ff2d748e458903c1553e81f1260073d6a6fa
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-02 10:36:23 +0545

    bug fix: removed headers from axios instance to solve Bearer null error

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -5,9 +5,6 @@ import axios, { AxiosInstance } from "axios";
 const instance: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_APP_BACKEND_URL,
     timeout: import.meta.env.VITE_APP_TIME_OUT,
-    headers: {
-        Authorization: `Bearer ${EncryptDecrypt.decrypt(localStorage.getItem("accessToken") as string)}`
-    }
 })
 
 export default instance

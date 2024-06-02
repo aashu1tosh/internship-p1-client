1334111ec164bf0a4c44c079662a68d42d9a9942
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-02 10:34:04 +0545

    feat: Completely changed how toast works created class to support multiple toast at a time

--- src/ui/atom/Toast/Toast.tsx
+++ src/ui/atom/Toast/Toast.tsx
@@ -1,34 +1,71 @@
-// Toast.tsx
-import React, { useEffect, useState } from 'react';
+// // Toast.tsx
+// import React, { useEffect, useState } from 'react';
+// import './Toast.css';
+
+// type ToastProps = {
+//   message: string;
+//   type: 'success' | 'error' | 'info';
+// };
+
+// const Toast: React.FC<ToastProps> = ({ message, type }) => {
+//   const [showToast, setShowToast] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setShowToast(false);
+//     }, 5000);
+
+//     return () => {
+//       clearTimeout(timer);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       {showToast && (
+//         <div className={`toast ${type}`}>
+//           <p>{message}</p>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Toast;
+
+import React, { useEffect } from "react";
 import './Toast.css';
+export interface ToastProps {
+  id: string;
+  destroy: () => void;
+  title: string;
+  content: string;
+  duration?: number;
+  type: 'success' | 'error' | 'warning' | 'info';
+}
 
-type ToastProps = {
-    message: string;
-    type: 'success' | 'error' | 'info';
-};
+const Toast: React.FC<ToastProps> = (props) => {
+  const { destroy, content, title, duration = 0, id, type } = props;
 
-const Toast: React.FC<ToastProps> = ({ message, type }) => {
-    const [showToast, setShowToast] = useState(true);
+  useEffect(() => {
+    if (!duration) return;
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowToast(false);
-        }, 10000);
+    const timer = setTimeout(() => {
+      destroy();
+    }, duration);
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, []);
+    return () => clearTimeout(timer);
+  }, [destroy, duration]);
 
-    return (
-        <>
-            {showToast && (
-                <div className={`toast ${type}`}>
-                    <p>{message}</p>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <div className={`${type}`}>
+      <div className={"toast-header"}>
+        <div key={id}>{title}</div>
+        <button onClick={destroy}>X</button>
+      </div>
+      <div className={"toast-body"}>{content}</div>
+    </div>
+  );
 };
 
 export default Toast;

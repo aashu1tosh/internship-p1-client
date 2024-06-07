6d533c2de01014172344d3890a2d187643141e3c
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-07 16:06:49 +0545

    feat: Added Pagination to the page

--- src/Components/AdminList/AdminList.tsx
+++ src/Components/AdminList/AdminList.tsx
@@ -1,20 +1,34 @@
 import axios from '@services/instance'
 import AdminDetails from 'Components/AdminDetails/AdminDetails'
+import Pagination from 'Components/Pagination/Pagination'
 import UpdateAdmin from 'Components/UpdateAdmin/UpdateAdmin'
-import { UserType } from 'interface/global.interface'
+import { PaginationInterface, SearchObject, UserInterface, UserType, defaultPagination } from 'interface/global.interface'
 import { useEffect, useState } from 'react'
-import { FaEdit } from 'react-icons/fa'
+import { useForm } from 'react-hook-form'
+import { FaCaretDown, FaCaretUp, FaEdit, FaSearch } from 'react-icons/fa'
 import { MdDelete } from 'react-icons/md'
+import { TbCaretUpDownFilled } from 'react-icons/tb'
+import InputField from 'ui/atom/InputField/InputField'
 import { toast } from 'ui/atom/Toast/ToastManager'
 import Dialog from 'ui/organism/Modal/Modal'
 import './AdminList.css'
 
 const AdminList = () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { register, handleSubmit } = useForm<any>();
+
     const [userData, setUserData] = useState<UserType>([]);
     const [viewDialogOpen, setViewDialogOpen] = useState<boolean>(false);
     const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>("");
 
     const [id, setId] = useState<string>("");
+    const [sortStatus, setSortStatus] = useState<0 | 1 | 2>(0);
+
+    const [specificUser, setSpecificUser] = useState<UserInterface>();
+
+    const [totalPages, setTotalPages] = useState<PaginationInterface | undefined>();
+    const [refresh, setRefresh] = useState<boolean>(false);
 
     const closeViewDialog = () => {
         setViewDialogOpen(false);
@@ -22,15 +36,20 @@ const AdminList = () => {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get('/admin');
+            const response = await axios.get(`/admin?search=${search}&page=${totalPages?.currentPage || 1}`);
             console.log(response.data.data.data);
             setUserData(response.data.data.data);
+            console.log(response.data.data.pagination)
+            if (response.data.data.pagination.totalPages > 1) {
+                console.log("Show more pages")
+            }
+            setTotalPages(response.data.data.pagination);
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
         } catch (error: any) {
             console.error(error);
             toast.show({
                 title: 'Operation Failed',
-                content: error?.response.data.message,
+                content: error?.response?.data?.message,
                 duration: 5000,  // Duration in milliseconds,
                 type: 'error'
             });
@@ -65,15 +84,16 @@ const AdminList = () => {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [refresh]);
 
     function handleRowClick(_id: string): void {
         // navigate(`/admin/${id}`);
         setViewDialogOpen(true);
         setId(_id);
     }
-    function updateAdminClick(_id: string): void {
+    function updateAdminClick(_id: string, user: UserInterface): void {
         setId(_id);
+        setSpecificUser(user);
         setDialogOpen(true);
     }
 
@@ -84,14 +104,70 @@ const AdminList = () => {
         setDialogOpen(false);
     };
 
+    const sort = () => {
+        setSortStatus(prevStatus => {
+            const newStatus = prevStatus === 0 ? 1 : prevStatus === 1 ? 2 : 0;
+            const sortedList = [...userData].sort((a, b) => {
+                const firstNameA = a.details.firstName.en.toUpperCase() || "";
+                const firstNameB = b.details.firstName.en.toUpperCase() || "";
+
+                if (newStatus === 1) {
+                    return firstNameA.localeCompare(firstNameB);
+
+                } else if (newStatus === 2) {
+                    return firstNameB.localeCompare(firstNameA);
+                } else
+                    return 0;
+            });
+            setUserData(sortedList)
+            return newStatus;
+        });
+    }
+
+    const handleUserUpdate = (updatedUser: UserInterface) => {
+        setUserData((prevUserData) =>
+            prevUserData.map((user) => (user.id === updatedUser.id ? updatedUser : user))
+        );
+        closeDialog();
+    };
+
+    const searchFn = (data: SearchObject) => {
+        try {
+            console.log(data)
+            setSearch(data.search);
+            setRefresh(prev => !prev)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <>
+            <div className='search-box'>
+                <form onSubmit={handleSubmit(searchFn)}>
+                    <InputField
+                        icon={<FaSearch />}
+                        placeholder="Search Here"
+                        label=""
+                        name='search'
+                        register={register}
+                    />
+                </form>
+            </div>
             <div className='admin-list'>
+
                 <table className='admin-list-table'>
                     <thead>
                         <tr>
                             <th>S.No.</th>
-                            <th>First Name</th>
+                            <th onClick={sort} className='first-name'>
+                                First Name
+                                <div className='arrow-icons'>
+                                    {sortStatus === 0 && <TbCaretUpDownFilled />}
+                                    {sortStatus === 1 && <FaCaretDown />}
+                                    {sortStatus === 2 && <FaCaretUp />}
+                                </div>
+                            </th>
                             <th>Last Name</th>
                             <th>Phone Number</th>
                             <th>Email</th>
@@ -113,7 +189,7 @@ const AdminList = () => {
                                 <td>{user?.role.toString().replace('_', ' ').toLowerCase()}</td>
                                 <td>{user?.username}</td>
                                 {/* <Link to={`/admin/update-admin/${user?.id}`}> </Link>*/}
-                                <td title='Edit' onClick={(e) => { e.stopPropagation(); updateAdminClick(user?.id) }}>  <FaEdit size={16} color='green' cursor={'pointer'} /></td>
+                                <td title='Update' onClick={(e) => { e.stopPropagation(); updateAdminClick(user?.id, user) }}>  <FaEdit size={16} color='green' cursor={'pointer'} /></td>
                                 <td onClick={(e) => { e.stopPropagation(); deleteUser(user?.id); }}><MdDelete size={21} color='red' /></td>
                             </tr>
                         ))
@@ -122,16 +198,26 @@ const AdminList = () => {
 
                 </table>
 
+                {
+                    totalPages?.totalPages || 0 > 1 ?
+                        <Pagination
+                            setTotalPages={setTotalPages as React.Dispatch<React.SetStateAction<PaginationInterface>>}
+                            pagination={totalPages ?? defaultPagination}
+                            setRefresh={setRefresh}
+                        /> : <></>
+                }
+
                 <Dialog open={viewDialogOpen} onClose={closeViewDialog}>
                     <AdminDetails id={id} />
                 </Dialog>
 
                 <Dialog open={dialogOpen} onClose={closeDialog}>
+
                     <UpdateAdmin
                         id={id}
                         closeDialog={closeDialog}
-                        userData={userData}
-                        setUserData={setUserData}
+                        user={specificUser}
+                        handleUserUpdate={handleUserUpdate}
                     />
 
                 </Dialog>

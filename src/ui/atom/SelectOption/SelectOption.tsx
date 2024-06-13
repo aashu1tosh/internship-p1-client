9db80b65b1ecac172c31ae82b0b96aaa9d612faa
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-13 17:00:41 +0545

    remove default selected hidden option

--- src/ui/atom/SelectOption/SelectOption.tsx
+++ src/ui/atom/SelectOption/SelectOption.tsx
@@ -10,6 +10,7 @@ interface SelectOptionProps {
     name: string;
     option?: OptionInterface[];
     register: UseFormRegister<any>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     options: any;
     error?: FieldError;
 }
@@ -20,7 +21,7 @@ const SelectOption: React.FC<SelectOptionProps> = ({ label = "", name, option, r
             <label htmlFor={name}>{label}<span className='red-text'>{options?.required ? "*" : ""}</span></label>
 
             <select id={name} {...register(name, options)} className={error ? 'input-error' : ''} >
-                <option value="" disabled selected hidden>Please Choose...</option>
+                {/* <option value="" disabled selected hidden>Please Choose...</option> */}
                 {
                     option && option.map((data, index) => (
                         <option value={data?.value} key={index}>{data?.label}</option>
